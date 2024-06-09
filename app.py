e9d751a4fbeda394988525b19bb53a4a376db617
Venkatesh Parameswaran <131514161+Venkatesh-Parameswaran@users.noreply.github.com>
2024-06-09 12:26:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ pipe = pickle.load(open('pipe.pkl', 'rb'))
 df = pd.read_csv('new_laptop.csv')
 
 # Set the background image URL
-background_image_url = "https://raw.githubusercontent.com/Venkatesh-Parameswaran/Laptop-Price-Predictor/main/laptop%20bk6.jpg"
+background_image_url = "https://raw.githubusercontent.com/Venkatesh-Parameswaran/Laptop-Price-Predictor/main/laptop%20bk2.jpg"
 
 # Add some styling to the page
 st.markdown(
@@ -39,7 +39,7 @@ st.markdown(
         color: #000000; /* Black font color for markdown */
     }}
     .stMarkdown a {{
-        color: #FF4500; /* Orange font color for links */
+        color: #0000FF; /* Blue font color for links */
     }}
     .stAlert > div > div > div > div, .stException > div > div > div > div {{
         color: #000000 !important; /* Black font color for alerts and exceptions */
