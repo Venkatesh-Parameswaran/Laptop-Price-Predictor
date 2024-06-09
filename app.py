c1ad14cbb800c40e8c16192cc473e7285125eac0
Venkatesh Parameswaran <131514161+Venkatesh-Parameswaran@users.noreply.github.com>
2024-06-09 23:38:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown(
     body {{
         background-image: url("{background_image_url}");
         background-size: cover;
-        background-position: right;
+        background-position: left;
         background-attachment: fixed;
         color: white;
     }}
