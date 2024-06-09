afcac414cfa60e5cddd05b0702e7905cbe0cbd8e
Venkatesh Parameswaran <131514161+Venkatesh-Parameswaran@users.noreply.github.com>
2024-06-09 20:50:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.markdown(
         color: white;
     }}
     .stApp {{
-        background-color: rgba(100, 100, 100, 0.5); /* Semi-transparent background for Streamlit elements */
+        background-color: rgba(75, 75, 75, 0.5); /* Semi-transparent background for Streamlit elements */
         color: white; /* White font color */
         font-size: 18px; /* Font size */
         font-family: Arial, sans-serif; /* Font family */
