b49f43796f652dbf3273a2dcf7cc05f8deb005a2
Venkatesh Parameswaran <131514161+Venkatesh-Parameswaran@users.noreply.github.com>
2024-06-09 18:31:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ df = pd.read_csv('new_laptop.csv')
 # background_image_url = "https://raw.githubusercontent.com/Venkatesh-Parameswaran/Laptop-Price-Predictor/main/laptop%20bk11.jpg"
 
 # Set the background image URL
-background_image_url = "https://i.pinimg.com/564x/8d/24/64/8d2464b682957854aae5217e50f167c7.jpg"
+background_image_url = "https://i.pinimg.com/564x/40/2d/d1/402dd15b276549d4d19ce46181476fd9.jpg"
 
 # Add some styling to the page
 st.markdown(
